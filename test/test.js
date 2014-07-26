11fb6f4d33672cb908c0d5e73ffb29d160fca9cc
Sebastian Bauer <gjlnetwork@googlemail.com>
2014-07-27 00:25:27 +0200

    Extended test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 var assert = require('assert');
 var parseString = require('xml2js').parseString;
 var parameterExtractor = require('../parameter-extractor.js');
+var pluginManager = require('../plugin-manager.js');
+
+describe('Plugin Manager', function(){
+	describe('Plugin Loader', function(){
+		it('Should find all plugins', function(done){
+			pluginManager.loadPlugins();
+			assert.equal(2, Object.keys(pluginManager.pluginList).length);
+			done();
+		});
+	});
+});
 
 describe('XML Parsing', function(){
 	var xmlString = '<?xml version="1.0" ?><methodCall><methodName>metaWeblog.newPost</methodName><params><param><value><string></string></value></param><param><value><string>meinuser</string></value></param><param><value><string>meinpw</string></value></param><param><value><struct><member><name>title</name><value><string>Title</string></value></member><member><name>description</name><value><string>wakeonlan</string></value></member><member><name>categories</name><value><array><data><value><string>broadcast=192.168.178.255</string></value><value><string>mac=00:25:22:A2:84:8D</string></value></data></array></value></member><member><name>mt_keywords</name><value><array><data><value><string>tag1</string></value></data></array></value></member><member><name>post_status</name><value><string>publish</string></value></member></struct></value></param><param><value><boolean>1</boolean></value></param></params></methodCall>';
+	var xmlStringBadParams = '<?xml version="1.0" ?><methodCall><methodName>metaWeblog.newPost</methodName><params><param><value><string></string></value></param><param><value><string>meinuser</string></value></param><param><value><string>meinpw</string></value></param><param><value><struct><member><name>title</name><value><string>Title</string></value></member><member><name>description</name><value><string>wakeonlan</string></value></member><member><name>categories</name><value><array><data><value><string>broadcast=192.=168.178.255</string></value><value><string>mac=00:25:22:A2:84:8D</string></value></data></array></value></member><member><name>mt_keywords</name><value><array><data><value><string>tag1</string></value></data></array></value></member><member><name>post_status</name><value><string>publish</string></value></member></struct></value></param><param><value><boolean>1</boolean></value></param></params></methodCall>';
 	
 	describe('Parameter Extractor', function(){
 		it('Should extract all parameters correctly', function(done){
@@ -25,7 +37,19 @@ describe('XML Parsing', function(){
 				done();
 			});
 		});
+		it('Should throw an exception when the parameters are incorrectly set', function(done){
+			parseString(xmlStringBadParams, {
+	      explicitArray: true,
+	      normalize: true,
+	      normalizeTags: true,
+	      trim: true
+			},
+			function (err, result) {
+			  assert.throws(function(){
+					parameterExtractor.extractParameters(result.methodcall.params[0]);
+				}, Error, 'Parameters not valid!');
+				done();
+			});
+		});
 	});
-	
-
 });
