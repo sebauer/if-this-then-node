6948eb0c87776a139efc6b9600178fa7bd01039a
Sebastian Bauer <gjlnetwork@googlemail.com>
2014-07-26 18:31:47 +0200

    Updated WOL plugin

--- plugins/wakeonlan.js
+++ plugins/wakeonlan.js
@@ -2,10 +2,14 @@ var exec = require('child_process').exec;
 
 module.exports = {
   run: function (params) {
-    console.log(params);
+		var command = 'wakeonlan -i '+params.broadcast+' '+params.mac;
+		console.log(command);
+		exec(command, function callback(error, stdout, stderr){
+    	console.log(stdout);
+		});
   },
 	info: function() {
-		return 'IFTTN Wake-On-LAN Module V0.1';
+		return 'IFTTN Wake-On-LAN Plugin V0.1';
 	}
 };
 
