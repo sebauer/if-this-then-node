998cbaaf8d2fb4ef5170da176387364ae90fc346
Sebastian Bauer <gjlnetwork@googlemail.com>
2014-08-05 09:42:12 +0200

    Using redis sets instead of single keys

--- plugins/limitless-onleave-autooff.js
+++ plugins/limitless-onleave-autooff.js
@@ -1,6 +1,5 @@
 var led = require('limitless-gem');
 var redis = require('redis');
-var client = redis.createClient();
 var format = require('util').format;
 
 module.exports = {
@@ -17,24 +16,13 @@ module.exports = {
 
     if(params.enterexit.toLowerCase() == 'entered' || params.enterexit.toLowerCase() == 'connected to') {
         log.info('Client %s is coming home', params.clientname);
-
-        log.info('Registered %s as being home', params.clientname);
-        client.set('loa-'+params.clientname, true);
-
+        registerClient(params.clientname);
     } else if(params.enterexit.toLowerCase() == 'exited' || params.enterexit.toLowerCase() == 'disconnected from') {
         log.info('"%s" has left the building', params.clientname);
-
-        // Remove client from store
-        client.del('loa-'+params.clientname, function(err, reply) {
-            // Now check if there are some clients left
-            log.info('Checking for remaining clients..');
-            client.keys('loa-*', function(err, replies) {
-                if(replies.length == 0) {
-                    log.info('All clients left, turning off ALL lights');
-                    connection.send(led.RGBW.ALL_OFF);
-                }
-            });
-        });
+        deregisterClient(params.clientname, function(){
+            log.info('All clients left, turning off ALL lights');
+            connection.send(led.RGBW.ALL_OFF);
+        })
     }
     // do whatever you want in this plugin
 	callback({
@@ -46,3 +34,24 @@ module.exports = {
 		return 'IFTTN LimitlessLED Plugin - onLeave: auto-off';
 	}
 };
+
+var registerClient = function(clientname) {
+    var client = redis.createClient();
+    log.info('Registered %s as being home', clientname);
+    client.sadd('ifttn-limitless-clients', clientname);
+}
+
+var deregisterClient = function(clientname, callback) {
+    var client = redis.createClient();
+    // Remove client from store
+    client.srem('ifttn-limitless-clients', clientname, function(err, reply) {
+        // Now check if there are some clients left
+        log.info('Checking for remaining clients..');
+        client.smembers('ifttn-limitless-clients', function(err, replies) {
+            // If no clients are left, execute the callback function
+            if(replies.length == 0) {
+                callback();
+            }
+        });
+    });
+}
