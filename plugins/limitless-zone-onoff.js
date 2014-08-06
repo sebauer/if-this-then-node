9c7a72ed0220d5993e736a7e9e228a79fec4caf5
Sebastian Bauer <gjlnetwork@googlemail.com>
2014-08-06 23:32:00 +0200

    Fixed indentation

--- plugins/limitless-zone-onoff.js
+++ plugins/limitless-zone-onoff.js
@@ -1,26 +1,26 @@
 var led = require('limitless-gem');
 
 module.exports = {
-  run: function (params, log, callback) {
+    run: function (params, log, callback) {
 
-	var connection = led.createSocket({
-	        host: params.host,
-			port: params.port
-		},
-		'udp',
-		function () {
-			log.info('Connected to LimitlessLED %s:%d', params.host, params.port);
-		}
-	);
-    var cmd = switchLED(connection, params.zone, params.onoff);
-	callback({
-		'success' : true,
-		'output'  : 'Sent command '+cmd
-	});
-  },
-	info: function() {
-		return 'IFTTN LimitlessLED Plugin - Zone x On/Off';
-	}
+        var connection = led.createSocket({
+                host: params.host,
+                port: params.port
+            },
+            'udp',
+            function () {
+                log.info('Connected to LimitlessLED %s:%d', params.host, params.port);
+            }
+        );
+        var cmd = switchLED(connection, params.zone, params.onoff);
+        callback({
+            'success' : true,
+            'output'  : 'Sent command '+cmd
+        });
+    },
+    info: function() {
+        return 'IFTTN LimitlessLED Plugin - Zone x On/Off';
+    }
 };
 
 var switchLED = function(connection, zone, onoff) {
