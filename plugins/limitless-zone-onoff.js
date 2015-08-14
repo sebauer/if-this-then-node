b5174072441bc456cc4caf611d8e55c880853149
Sebastian Bauer <gjlnetwork@gmail.com>
2015-08-14 13:23:24 +0200

    Changed code style

--- plugins/limitless-zone-onoff.js
+++ plugins/limitless-zone-onoff.js
@@ -1,41 +1,42 @@
+/* global log */
 var led = require('limitless-gem');
 
 module.exports = {
-    run: function (params, log, callback) {
+  run: function (params, log, callback) {
 
-        var connection = led.createSocket({
-                host: params.host,
-                port: params.port
-            },
-            'udp',
-            function () {
-                log.info('Connected to LimitlessLED %s:%d', params.host, params.port);
-            }
-        );
-        var cmd = switchLED(connection, params.zone, params.onoff);
-        callback({
-            'success' : true,
-            'output'  : 'Sent command '+cmd
-        });
+    var connection = led.createSocket({
+      host: params.host,
+      port: params.port
     },
-    info: function() {
-        return 'IFTTN LimitlessLED Plugin - Zone x On/Off';
+    'udp',
+    function () {
+        log.info('Connected to LimitlessLED %s:%d', params.host, params.port);
+      }
+    );
+    var cmd = switchLED(connection, params.zone, params.onoff);
+    callback({
+      'success': true,
+      'output': 'Sent command ' + cmd
+    });
+  },
+  info: function () {
+      return 'IFTTN LimitlessLED Plugin - Zone x On/Off';
     }
 };
 
-var switchLED = function(connection, zone, onoff) {
-    var cmd = '';
-    switch(onoff.toUpperCase()) {
-        case('ON'):
-            cmd = led.RGBW['GROUP'+zone+'_ON'];
-            break;
-        case('OFF'):
-            cmd = led.RGBW['GROUP'+zone+'_OFF'];
-            break;
-        default:
-            log.warn('Error with command, input %s invalid', params.onoff);
-            break;
-    }
-    connection.send(cmd);
-    return cmd;
-}
+var switchLED = function (connection, zone, onoff) {
+  var cmd = '';
+  switch (onoff.toUpperCase()) {
+    case ('ON'):
+      cmd = led.RGBW['GROUP' + zone + '_ON'];
+      break;
+    case ('OFF'):
+      cmd = led.RGBW['GROUP' + zone + '_OFF'];
+      break;
+    default:
+      log.warn('Error with command, input %s invalid', onoff);
+      break;
+  }
+  connection.send(cmd);
+  return cmd;
+};
