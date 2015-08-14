282f6206113db0580c1ce4fb85b38261ac729aea
Sebastian Bauer <gjlnetwork@gmail.com>
2015-08-14 15:56:38 +0200

    Updated default config

--- config.js
+++ config.js
@@ -2,14 +2,16 @@ var config = {
   'user': 'seb',
   'pw': 'mypw',
   'host': '', // Put host name or ip address of light gateway
-  'port': '8899' // If necessary change the port of the light gateway. Default is 8899 
+  'port': '8899' // If necessary change the port of the light gateway. Default is 8899
 };
 
 module.exports = {
   getConfig: function () {
     var returnObj = {
       'user': config.user,
-      'pw': config.pw
+      'pw': config.pw,
+      'host': config.host,
+      'port': config.port
     };
     return returnObj;
   }
